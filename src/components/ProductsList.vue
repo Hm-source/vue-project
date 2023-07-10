a2704d01ce6a655f42ff27a2504f02f6dea40239
hyomin <hm0704vip@incross.com>
2023-07-10 16:55:09 +0900

    axios로 다시 수정

--- src/components/ProductsList.vue
+++ src/components/ProductsList.vue
@@ -1,14 +1,17 @@
 <script setup>
-import { useCounterStore } from '@/stores/counter'
-const counter = useCounterStore()
-counter.getProducts()
+// import { useCounterStore } from '@/stores/counter'
+// const counter = useCounterStore()
+// counter.getProducts()
+import axios from 'axios'
+const { data } = await axios.get('https://fakestoreapi.com/products')
+const products = data
 </script>
 
 <template>
   <div class="store-products">
     <h1>products</h1>
     <div>
-      <li v-for="(product, index) in counter.products" :key="index">
+      <li v-for="(product, index) in products" :key="index">
         <div class="product">
           <img :src="product.image" width="40px" height="40px" :alt="product.title" />
           <div>
