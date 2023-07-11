71047e37112abd8c455ea970428799beac52324c
hyomin <hm0704vip@incross.com>
2023-07-11 15:36:24 +0900

    store 수정

---
+++ src/stores/product.js
@@ -0,0 +1,20 @@
+import { ref, computed } from 'vue'
+import { defineStore } from 'pinia'
+import axios from 'axios'
+export const useProductStore = defineStore('product', () => {
+  const count = ref(0)
+  const products = ref([])
+  const doubleCount = computed(() => count.value * 2)
+  function increment() {
+    count.value++
+  }
+  async function getProducts() {
+    if (products.value.length > 0) {
+      return products.value
+    } else {
+      const { data } = await axios.get('https://fakestoreapi.com/products')
+      products.value = data
+    }
+  }
+  return { count, products, doubleCount, increment, getProducts }
+})
