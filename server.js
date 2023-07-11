5069e1905fbb6f411f1498b895d5df7f644202e4
hyomin <hm0704vip@incross.com>
2023-07-11 15:35:19 +0900

    server 주석 추가

--- server.js
+++ server.js
@@ -70,21 +70,31 @@ export async function createServer(
 
       // index.html의 app 부분에 rendered content 넣음.
       if (!isProd) {
+        // 1. index.html 파일을 읽어들입니다.
         template = fs.readFileSync(resolve("index.html"), "utf-8");
+        // 2. Vite의 HTML 변환 작업을 통해 Vite HMR 클라이언트를 주입하고,
+        //    Vite 플러그인의 HTML 변환도 적용합니다.
         template = await vite.transformIndexHtml(url, template);
+        // 3. 서버의 진입점(Entry)을 로드합니다.
+        //    vite.ssrLoadModule은 Node.js에서 사용할 수 있도록 ESM 소스 코드를 자동으로 변환합니다.
+        //    추가적인 번들링이 필요하지 않으며, HMR과 유사한 동작을 수행합니다.
+
         render = (await vite.ssrLoadModule("/src/entry-server.js")).render;
       } else {
         template = indexProd;
         render = (await import("./dist/server/entry-server.js")).render;
       }
-
-      const [appHtml, productListHtml, preloadLinks] = await render(url, manifest);
-      // 렌더링된 HTML을 템플릿에 주입합니다.
+      console.log('server.js',url, manifest);
+      // 4. 앱의 HTML을 렌더링합니다.
+        //    이는 entry-server.js에서 내보낸(Export) `render` 함수가
+        //    renderToString()과 같은 적절한 프레임워크의 SSR API를 호출한다고 가정합니다.
+      const [appHtml, preloadLinks] = await render(url, manifest);
+      // 5. 렌더링된 HTML을 템플릿에 주입합니다.
       const html = template
         .replace(`<!--preload-links-->`, preloadLinks)
-        .replace(`<!--app-html-->`, appHtml)
-        .replace(`<!--product-list-->`, productListHtml);
-      // 렌더링된 HTML을 응답으로 전송합니다.
+        .replace(`<!--app-html-->`, appHtml);
+        // .replace(`<!--product-list-->`, productListHtml);
+      // 6. 렌더링된 HTML을 응답으로 전송합니다.
       res.status(200).set({ "Content-Type": "text/html" }).end(html);
     } catch (e) {
       vite && vite.ssrFixStacktrace(e);
@@ -92,14 +102,12 @@ export async function createServer(
       res.status(500).end(e.stack);
     }
   });
-
   return { app, vite };
 }
 
 if (!isTest) {
   createServer().then(({ app }) =>
     app.listen(6173, () => {
-      console.log(process.env.NODE_ENV);
       console.log("http://localhost:6173");
     })
   );
