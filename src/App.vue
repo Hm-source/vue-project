ae117790c1a558a517e6abe7211832bbf358ea2c
hyomin <hm0704vip@incross.com>
2023-07-11 15:36:08 +0900

    css 수정

--- src/App.vue
+++ src/App.vue
@@ -1,90 +1,68 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-// import HelloWorld from './components/HelloWorld.vue'
-import { useCounterStore } from '@/stores/counter'
-const counter = useCounterStore()
+import { useProductStore } from '@/stores/product'
+const product = useProductStore()
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-    <div>
-      count:{{ counter.count }}
-      <button @click="counter.increment">up</button>
+    <div class="header-content">
+      <div class="logo-container">
+        <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+      </div>
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+      <div class="count-container">
+        count: {{ product.count }}
+        <button @click="product.increment">up</button>
+      </div>
     </div>
-    <div class="wrapper">
-      <!-- <HelloWorld msg="You did it!" /> -->
-    </div>
+    <RouterView />
   </header>
-  <footer>
-    <nav>
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/about">About</RouterLink>
-    </nav>
-  </footer>
-  <RouterView />
 </template>
 
-<style scoped>
+<style>
 header {
-  line-height: 1.5;
-  max-height: 100vh;
+  width: 100%;
+  background-color: rgb(255, 252, 252);
+}
+
+.header-content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.logo-container {
+  margin-bottom: 20px;
 }
 
 .logo {
-  display: block;
-  margin: 0 auto 2rem;
+  width: 125px;
+  height: 125px;
 }
 
 nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
+  display: flex;
+  align-items: center;
+  padding: 20px;
 }
 
 nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
+  margin: 10px;
+  padding: 10px;
+  text-decoration: none;
+  color: #333;
 }
 
-nav a:first-of-type {
-  border: 0;
+.count-container {
+  display: flex;
+  align-items: center;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+button {
+  margin-left: 10px;
 }
 </style>
